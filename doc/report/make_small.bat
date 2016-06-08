79dee9d4202e175ace60a19768ae860291c23c88
David Riddervold Marconis <david@marconis.no>
2016-06-08 02:27:27 +0200

    Doc: Started writing the architecture

---
+++ doc/report/make_small.bat
@@ -0,0 +1,3 @@
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+set today=%MyDate:~0,4%%MyDate:~4,2%%MyDate:~6,2%
+gswin64 -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -r600 -sOutputFile=AlternativeGames%today%.pdf report.pdf
